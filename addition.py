5adc79118c902d0efe65fd5d6f21c5c6cc4e7718
Meowmeow-alt <baotiendancer@gmail.com>
2023-11-02 15:53:01 +0700

    App update

--- addition.py
+++ addition.py
@@ -1,10 +1,11 @@
 from functools import wraps
-from flask import g, request, redirect, url_for
+from flask import g, request, redirect, session
+
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None:
-            return redirect(url_for('login', next=request.url))
+        if session.get("user_id") is None:
+            return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-import sqlite3
-from flask import Flask, render_template, redirect, request, session
+from cs50 import SQL
+from flask import Flask, render_template, redirect, request, session, flash, g
 from werkzeug.security import check_password_hash, generate_password_hash
 import secrets
 from addition import login_required
 
-db = sqlite3.connect("user.db")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
+
+db = SQL("sqlite:///user.db")
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 @app.route('/', methods=['GET','POST'])
@@ -18,17 +27,16 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
     session.clear()
+
     if request.method == 'POST':
         username = request.form.get("username")
         password = request.form.get("password")
 
-        if not username:
-            return "must provide username", 403
-        if not password:
-            return "must provide password", 403
+        if not username or not password:
+            return "must provide username/password", 403
         
         rows = db.execute("SELECT * FROM users WHERE username = ?", username)
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+        if len(rows) != 1 or not check_password_hash(rows[0]["password"], password):
             return "invalid username and/or password", 403
 
         # Remember which user has logged in
@@ -41,11 +49,29 @@ def login():
 @app.route('/logout', methods=['GET','POST'])
 @login_required
 def logout():
-    return render_template('logout.html')
+    session.clear()
+    return redirect("/")
 
 
 @app.route('/register', methods=['GET','POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirmation")
+
+        if not username or not password:
+            return "must provide username/password", 400
+        if password != confirm or not confirm:
+            flash("Password and confirmation do not match!")
+        usernames = [row['username'] for row in db.execute("SELECT * FROM users")]
+        if username == usernames:
+            flash("Username already exists")
+        
+        hashpass = generate_password_hash(password)
+        db.execute("INSERT INTO users (username, password) VALUES (?,?)", username, hashpass)
+        return redirect('/')
+    
     return render_template('register.html')
 
 
